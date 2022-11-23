77ece429c8cc2db9b1784df1f8dcb3a111d7d931
KrivanTomas <krivant.05@spst.eu>
2022-11-23 19:30:19 +0100

    class caching, not finished

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import { got } from 'got';
 import * as cheerio from 'cheerio';
 import * as fs from 'fs';
+import { type } from 'os';
 
 
 const LoadData = async () => {
     try {
-        const url = `https://bakalari.spst.cz/Timetable/Public/Actual/Class/7D`;
+        const url = `https://bakalari.spst.cz/Timetable/Public/`;
         const res = got(url);
         const data = await res;
         return cheerio.load(data.body);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 };
@@ -22,39 +23,41 @@ const LoadDataFrom = async (addUrl) => {
         const data = await res;
         return cheerio.load(data.body);
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 };
 
 const cacheLongtime = async () => {
-    console.log('-=-=-=-=-=-Caching longtime-=-=-=-=-=-')
+    console.log('\x1b[35m', '-=-=-=-=-=-Caching longtime-=-=-=-=-=-', '\x1b[0m')
+    let timed = Date.now();
     const $ = await LoadData();
     let classes = [], teachers = [], rooms = [];
-    try{ // CLASSES
+    try { // CLASSES
         $('option', '#selectedClass').each((i, elem) => { classes[i] = [elem.attribs['value'], $(elem).text().replace(/\s+/g, '')]; });
         classes = classes.slice(1);
         console.log('Cached ' + classes.length.toString() + ' classes');
-    } catch(err){ console.log(err); }
+    } catch (err) { console.log(err); }
 
-    try{ // TEACHERS
+    try { // TEACHERS
         $('option', '#selectedTeacher').each((i, elem) => { teachers[i] = [elem.attribs['value'], $(elem).text().replace(/\s{2,}/g, '')]; });
         teachers = teachers.slice(1);
         console.log('Cached ' + teachers.length.toString() + ' teachers');
-    } catch(err){ console.log(err); }
-    
-    try{ // ROOMS
+    } catch (err) { console.log(err); }
+
+    try { // ROOMS
         $('option', '#selectedRoom').each((i, elem) => { rooms[i] = [elem.attribs['value'], $(elem).text().replace(/\s+/g, '')]; });
         rooms = rooms.slice(1);
         console.log('Cached ' + rooms.length.toString() + ' rooms');
-    } catch(err){ console.log(err); }
+    } catch (err) { console.log(err); }
 
-    try{ // cache into a .json file
-        if(!fs.existsSync('cache')) fs.mkdirSync('cache')
-        let ctrInfo = JSON.stringify({ classes: classes, teachers: teachers, rooms: rooms});
+    try { // cache into a .json file
+        if (!fs.existsSync('cache')) fs.mkdirSync('cache');
+        let ctrInfo = JSON.stringify({ classes: classes, teachers: teachers, rooms: rooms });
         fs.writeFileSync("cache/longtime.json", ctrInfo);
-        console.log('✔ Cached and saved to file ✔');
-    } catch(err){ console.log(err); }
+        timed = Date.now() - timed;
+        console.log('\x1b[32m', '✔ Cached and saved to file in ' + timed.toString() + 'ms ✔', '\x1b[0m');
+    } catch (err) { console.log(err); }
 };
 
 const getLongtimeCache = () => {
@@ -62,81 +65,198 @@ const getLongtimeCache = () => {
     return JSON.parse(data);
 };
 
-//setup code
-
-if(!fs.existsSync('cache/longtime.json')){
-    await cacheLongtime();
-}
-
-// 🔥🔥 code (not lit but on fire)
-
-
-const LoadCTRData = async (CTRvalue, ctr) => {
+const LoadCTRData = async (ctr, CTRvalue) => {
     const longtime = getLongtimeCache();
     let path;
-    switch(ctr){
+    switch (ctr) {
         case 'class': {
-            let value = longtime['classes'].filter((x) => {return x[1] == CTRvalue})[0];
+            let value = longtime['classes'].filter((x) => { return x[1] == CTRvalue })[0];
+            if (value === undefined) {
+                console.log('\x1b[31m', '⚠ No match found for argument >' + CTRvalue + '<', '\x1b[0m');
+                break;
+            }
             path = 'Actual/Class/' + value[0];
             break;
         }
         case 'teacher': {
-            let value = longtime['teachers'].filter((x) => {return x[1] == CTRvalue})[0];
+            let value = longtime['teachers'].filter((x) => { return x[1] == CTRvalue })[0];
+            if (value === undefined) {
+                console.log('\x1b[31m', '⚠ No match found for argument >' + CTRvalue + '<', '\x1b[0m');
+                break;
+            }
             path = 'Actual/Teacher/' + value[0];
             break;
         }
         case 'room': {
-            let value = longtime['rooms'].filter((x) => {return x[1] == CTRvalue})[0];
+            let value = longtime['rooms'].filter((x) => { return x[1] == CTRvalue })[0];
+            if (value === undefined) {
+                console.log('\x1b[31m', '⚠ No match found for argument >' + CTRvalue + '<', '\x1b[0m');
+                break;
+            }
             path = 'Actual/Room/' + value[0];
             break;
+        }
+        default: {
+            console.log('\x1b[31m', '⚠ Incorrect ctr argument >' + ctr + '<', '\x1b[0m');
         }
     }
     const $ = await LoadDataFrom(path);
     return $;
 }
 
+const GetCTRPair = (ctr, CTRvalue) => {
+    const data = fs.readFileSync('cache/longtime.json', 'utf8');
+    return JSON.parse(data)[ctr].filter(x => x[1] === CTRvalue)[0];
+};
 
-
-const $ = await LoadCTRData('ITA3', 'class');
-
-let hodiny = [];
-const actualDate = new Date();
-
-
-$('.day-item-hover').each((i, elem) => {
-    hodiny[i] = JSON.parse(elem.attribs['data-detail']);
-
-    let stuff = hodiny[i]['subjecttext'].match(/\d{1,2}/g);
-    hodiny[i]['time'] = [
+const ParseTime = (timeValues) => {
+    const timeNow = new Date();
+    return [
         new Date(
-        Date.parse(
-            actualDate.getFullYear().toString() + '-' +
-            stuff[1].toString() + '-' +
-            stuff[0].toString() + 'T' +
-            (stuff[3] < 10 ? '0' : '' ) + stuff[3].toString() + ':' + stuff[4].toString() + ':00.000Z')
+            Date.parse(
+                timeNow.getFullYear().toString() + '-' +
+                timeValues[1].toString() + '-' +
+                timeValues[0].toString() + 'T' +
+                (timeValues[3] < 10 ? '0' : '') + timeValues[3].toString() + ':' + timeValues[4].toString() + ':00.000Z')
         ),
         new Date(
-        Date.parse(
-            actualDate.getFullYear().toString() + '-' +
-            stuff[1].toString() + '-' +
-            stuff[0].toString() + 'T' +
-            (stuff[5] < 10 ? '0' : '' ) + stuff[5].toString() + ':' + stuff[6].toString() + ':00.000Z')
+            Date.parse(
+                timeNow.getFullYear().toString() + '-' +
+                timeValues[1].toString() + '-' +
+                timeValues[0].toString() + 'T' +
+                (timeValues[5] < 10 ? '0' : '') + timeValues[5].toString() + ':' + timeValues[6].toString() + ':00.000Z')
         )
     ];
-})
+}
+
+const CacheClassData = async (className) => {
+    console.log(`Started parsing ${className}`)
+    let timed = Date.now();
+    const $ = await LoadCTRData('class',className);
+
+    let lessons = [];
+    let readyForExp = [];
+    
+    $('.day-item-hover').each((i, elem) => {
+        lessons[i] = JSON.parse(elem.attribs['data-detail']);
+        //console.log(lessons);
+        readyForExp[i] = 
+        {
+            type: null,
+            subject: null,
+            lesson: null,
+            time: null,
+            teacher: null,
+            room: null,
+            group: null,
+            theme: null,
+            notice: null,
+            changeInfo: {
+                type: null,
+                subject: null,
+                teacher: null,
+                room: null
+            },
+            removedinfo: {
+                type: null,
+                subject: null,
+                teacher: null
+            }
+        };
+    
+        switch(lessons[i]['type']) {
+            case 'atom': {
+                readyForExp[i]['type'] = lessons[i]['type']; 
+                readyForExp[i]['subject'] = lessons[i]['subjecttext'].match(/^[^\|]+/)[0].slice(0,-1);
+                let timeValues = lessons[i]['subjecttext'].match(/-?\d{1,2}/g);
+                readyForExp[i]['lesson'] = timeValues[2];
+                readyForExp[i]['time'] = ParseTime(timeValues);
+                readyForExp[i]['teacher'] = lessons[i]['teacher']; 
+                readyForExp[i]['room'] = GetCTRPair('rooms',lessons[i]['room']); 
+                readyForExp[i]['group'] = lessons[i]['group'] === '' ? null : lessons[i]['group']; 
+                readyForExp[i]['theme'] = lessons[i]['theme'] === '' ? null : lessons[i]['theme']; 
+                readyForExp[i]['notice'] = lessons[i]['notice'] === '' ? null : lessons[i]['notice']; 
+                if(lessons[i]['changeinfo'] !== '') {
+                    let values = lessons[i]['changeinfo'].match(/[^\:,]+/g);
+                    readyForExp[i]['changeInfo']['type'] = values[0];
+                    switch(values[0]) {
+                        case 'Spojeno':{          
+                            if(values.length === 4){
+                                readyForExp[i]['changeInfo']['subject'] = values[1].slice(1);
+                                readyForExp[i]['changeInfo']['teacher'] = values[2].slice(1);
+                                readyForExp[i]['changeInfo']['room'] = GetCTRPair('rooms',values[3].slice(1));
+                            }
+                            if(values.length === 3){
+                                readyForExp[i]['changeInfo']['teacher'] = values[1].slice(1);
+                                readyForExp[i]['changeInfo']['room'] = GetCTRPair('rooms',values[2].slice(1));
+                            }
+                            break;
+                        }
+                        case 'Suplování':{
+                            readyForExp[i]['changeInfo']['teacher'] = values[1].slice(1);
+                            break;
+                        }
+                        case 'Změna místnosti':{
+                            readyForExp[i]['changeInfo']['room'] = GetCTRPair('rooms',values[1].slice(1));
+                            break;
+                        }
+                        default: {
+                            let msg = 'Unknown type in changed >' + values[0] + '< at >' + className + '<';
+                            console.log(msg);
+                            fs.writeFileSync('log/' + Date.now().toString() + '.log', msg);
+                        }
+                    }
+                }
+                break;
+            }
+            case 'removed': {
+                readyForExp[i]['type'] = lessons[i]['type'];
+                let timeValues = lessons[i]['subjecttext'].match(/\d{1,2}/g);
+                readyForExp[i]['time'] = ParseTime(timeValues);
+    
+                let values = lessons[i]['removedinfo'].match(/[^\(\),]+/g);
+                    readyForExp[i]['removedinfo']['type'] = values[0].slice(0,-1);
+                    switch(values[0].slice(0,-1)) {
+                        case 'Zrušeno':{          
+                            readyForExp[i]['removedinfo']['subject'] = values[1];
+                            readyForExp[i]['removedinfo']['teacher'] = values[2].slice(1);
+                            break;
+                        }
+                        default: {
+                            let msg = 'Unknown type in removed >' + values[0].slice(0,-1) + '< at >' + className + '<';
+                            console.log(msg);
+                            fs.writeFileSync('log/' + Date.now().toString() + '.log', msg);
+                        }
+                    }
+                break;
+            }
+            default: {
+                let msg = 'Unknown type >' + lessons[i]['type'] + '< at >' + className + '<';
+                console.log(msg);
+                fs.writeFileSync('log/' + Date.now().toString() + '.log', msg);
+            }
+        }
+    })
+    if (!fs.existsSync('cache')) fs.mkdirSync('cache');
+    if (!fs.existsSync('cache/classes')) fs.mkdirSync('cache/classes');
+    fs.writeFileSync(`cache/classes/${className}.json`, JSON.stringify(readyForExp));
+    timed = Date.now() - timed;
+    console.log(`Finished parsing ${className} in ${timed}ms`)
+}
 
 
+//setup code
 
-//console.log(hodiny);
+if (!fs.existsSync('cache/longtime.json')) {
+    await cacheLongtime();
+}
 
-let todayHodiny = [];
+//await CacheClassData('ENE4');
 
-todayHodiny = hodiny.filter((value, index, array) => {
-    if(value['time'][0].getDate() == actualDate.getDate()) {
-        return true;
-    }
-    return false;
+getLongtimeCache()['classes'].forEach(async (elem) => {
+    await CacheClassData(elem[1]);
 });
 
-//console.log(todayHodiny);
 
+
+// 🔥🔥 code (not lit but on fire)
